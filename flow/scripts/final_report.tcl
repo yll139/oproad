eea58444be476a5ebcc6dedaf18bc292ce39d3fd
yll <coding@MacPro.local>
2026-05-22 17:00:45 +0800

    chore: update all flow scripts and platform scripts

--- flow/scripts/final_report.tcl
+++ flow/scripts/final_report.tcl
@@ -38,7 +38,7 @@ if {[info exist ::env(RCX_RULES)]} {
 
   # Static IR drop analysis
   if {[info exist ::env(PWR_NETS_VOLTAGES)] && [string length $::env(PWR_NETS_VOLTAGES)] > 0} {
-    dict for {pwrNetName pwrNetVoltage}  {*}$::env(PWR_NETS_VOLTAGES) {
+    dict for {pwrNetName pwrNetVoltage} $::env(PWR_NETS_VOLTAGES) {
         set_pdnsim_net_voltage -net ${pwrNetName} -voltage ${pwrNetVoltage}
         analyze_power_grid -net ${pwrNetName} \
             -error_file $::env(REPORTS_DIR)/${pwrNetName}.rpt
@@ -47,7 +47,7 @@ if {[info exist ::env(RCX_RULES)]} {
     puts "IR drop analysis for power nets is skipped because PWR_NETS_VOLTAGES is undefined"
   }
   if {[info exist ::env(GND_NETS_VOLTAGES)] && [string length $::env(GND_NETS_VOLTAGES)] > 0} {
-    dict for {gndNetName gndNetVoltage}  {*}$::env(GND_NETS_VOLTAGES) {
+    dict for {gndNetName gndNetVoltage} $::env(GND_NETS_VOLTAGES) {
         set_pdnsim_net_voltage -net ${gndNetName} -voltage ${gndNetVoltage}
         analyze_power_grid -net ${gndNetName} \
             -error_file $::env(REPORTS_DIR)/${gndNetName}.rpt
